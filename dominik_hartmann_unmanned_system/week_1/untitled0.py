ea6fec0545a274c7a46f1e4d21238d89667def1e
Dominik <dh4cc@mail.umkc.edu>
2022-08-25 09:20:03 -0500

    Initial commit

---
+++ dominik_hartmann_unmanned_system/week_1/untitled0.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 24 11:10:40 2022
+
+@author: dhart
+"""
+import matplotlib as plt
+import numpy as np
+
+
+#def rowlen:
+
+def node_index(x, y, maxX, maxY, minX, minY, space):
+
+if__name__== '__main__': 
+    # Inputs:
+    maxX=10;
+    maxY=10;
+    minX=0;
+    minY=0;
+    space=0.5;
+    
+    # plot:
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax. set(xlim=(minX, maxX),
+            ylim=(minY, maxY))
+    
+    plt.show()
+                    
+    
